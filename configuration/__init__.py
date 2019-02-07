79b307a2e16ff777b4e6df7ce699d3434b867303
Hichem CHEBIHI <hichem-externe.chebihi@edf.fr>
2019-02-07 15:55:45 +0100

    add configuration with example to test

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -1,12 +1,24 @@
 import findspark
-import val as val
 
 findspark.init()
 
 from pyspark import SparkContext, SparkConf
 
-# val conf = new SparkConf()
-#              .setMaster("local[2]")
-#              .setAppName("CountingSheep")
-# val sc = new SparkContext(conf)
-val sc = new SparkContext(new SparkConf())
+conf = SparkConf().setAppName('MyFirstStandaloneApp').setMaster("local[4]").setAll(
+[('spark.executor.memory', '6g'),
+ ('spark.driver.memory', '4g'),
+ ('spark.executor.heartbeatInterval', '3s'),
+ ('spark.driver.extraJavaOptions', '-XX:+UseG1GC -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps'),
+ ('spark.executor.extraJavaOptions', '-XX:+UseG1GC -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps')])
+
+sc = SparkContext(conf=conf)
+
+
+text_file = sc.textFile("../shakespear.txt")
+
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
+             .reduceByKey(lambda a, b: a + b)
+
+print ("Number of elements: " + str(counts.count()))
+# counts.saveAsTextFile("./shakespeareWordCount.txt")
